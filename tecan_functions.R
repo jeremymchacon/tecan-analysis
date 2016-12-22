69e30b5930f288ff8bbebcdd61b61191c50b6a4b
Jeremy M. Chacon <jeremymchacon@gmail.com>
2016-12-22 14:17:45 -0600

    improved estimate for lag time for fitting baranyi curves

--- tecan_functions.R
+++ tecan_functions.R
@@ -130,11 +130,19 @@ get_baranyi_rate = function(results, well_ID, response_var = "OD", tries = 100){
   return(fit_baranyi(x, y, tries))
 }
 
+
+
 fit_baranyi = function(x, y, tries = 100){
   m1 <- NA
+  
+  #figure out start lag guess
+  lag_guess = guess_half_max(x,y)
   while(is.na(m1[1]) && (tries > 0)){
     m1 <- tryCatch( nls(y ~ baranyi(x, r, lag, ymax, y0),
-                        start = list(r = runif(1, 0.1, 0.4), lag = x[sample(-10:10, 1) + round(length(x) / 3)], ymax = max(y), y0 = min(y))),
+                        start = list(r = runif(1, 0.1, 0.4), 
+                                     lag = lag_guess + sample(-20:20, 1), 
+                                     ymax = max(y), 
+                                     y0 = min(y))),
                     error = function(e) return(NA))
     tries = tries - 1
   }
@@ -148,6 +156,23 @@ fit_baranyi = function(x, y, tries = 100){
   ymax = coef(m1)[3]
   y0 = coef(m1)[4]
   return(c(r, lag, ymax, y0))
+}
+
+guess_half_max = function(x, y){
+  # this function looks at a logistically-increasing time series
+  # and guesses when the growth rate is at a maximum (which is also when
+  # the trend is at half-max)
+  
+  # put in order
+  y = y[sort(x, index.return = TRUE)$ix]
+  x = sort(x)
+  
+  #find approximate time of max growth rate, using diffs, on smoothed y
+  y2 = diff(y)
+  y2 = zoo::rollmean(y2, 11, fill = NA, align = "center")
+  half_max_idx = which(y2 == max(y2, na.rm = TRUE))[1]
+  half_max = x[half_max_idx]
+  return(half_max)
 }
 
 baranyi <- function(t, r, lag, logymax, logy0){
